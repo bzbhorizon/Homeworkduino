e8f405597ca160838d65559ce59496c870d8e39c
bzbhorizon <bzb@cs.nott.ac.uk>
2010-05-07 20:06:31 +0100

    Shifted source; fixed build path

---
+++ src/bzb/se/ui/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bzb.se.ui;
+
+/**
+ * @author psxbdb
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello homework");
+	}
+
+}
