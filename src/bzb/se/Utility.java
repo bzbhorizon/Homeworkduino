f78157ee58a0b39b1331abe4346680030fb11d1b
bzbhorizon <bzb@cs.nott.ac.uk>
2010-09-16 17:00:24 +0100

    fixed line break

--- src/bzb/se/Utility.java
+++ src/bzb/se/Utility.java
@@ -30,7 +30,7 @@ public abstract class Utility {
 	public static void writeToLog (String line) {
 		try {
 		    BufferedWriter out = new BufferedWriter(new FileWriter("res/output.log"));
-		    out.write(new SimpleDateFormat("dd:mm:yyyy").format(Calendar.getInstance().getTime()) + ":" + line + "/r");
+		    out.write(new SimpleDateFormat("dd:mm:yyyy").format(Calendar.getInstance().getTime()) + ":" + line + "\r\n");
 		    out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
