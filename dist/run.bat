2aa0e547e6703982f29cfdc6ba197aee8f100c1c
bzbhorizon <bzb@cs.nott.ac.uk>
2010-09-16 16:45:46 +0100

    pushed dist

---
+++ dist/run.bat
@@ -0,0 +1,2 @@
+java -cp homeworkduino.jar -Djava.library.path=. bzb.se.ui.Command COM7 1
+pause
---
+++ dist/run.sh
@@ -0,0 +1,2 @@
+java -cp homeworkduino.jar -Djava.library.path=. bzb.se.ui.Command /dev/ttyUSB0 1
+pause
